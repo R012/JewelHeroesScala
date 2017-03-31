2284db15bc8ab9ea7183045105e482832ed899ef
R012 <mario.cobos@edu.uah.es>
2017-03-31 09:59:00 +0200

    Finished up a couple functions
    Also commented them :D

--- JewelHeroesScala/.worksheet/src/JewelHeroesScala.scala
+++ JewelHeroesScala/.worksheet/src/JewelHeroesScala.scala
@@ -1,6 +1,11 @@
 object JewelHeroesScala {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(69); 
-  println("Welcome to the Scala worksheet");$skip(557); 
+  println("Welcome to the Scala worksheet");$skip(765); 
   
+  /*
+  	EN: Function that prints the table to the screen. Still need to add a way to skip lines.
+  	ES: Función que escribe el tablero por pantalla. Todavía hay que añadir una forma de saltar
+  	líneas.
+  */
   def imprimir(tablero:List[Int]) : Boolean ={
   	if(tablero.isEmpty) return true
   	tablero.head match{
@@ -21,7 +26,41 @@ object JewelHeroesScala {;import org.scalaide.worksheet.runtime.library.Workshee
   		case 8 => print("B\t")
   		 imprimir(tablero.tail)
   	}
-  };System.out.println("""imprimir: (tablero: List[Int])Boolean""");$skip(50); 
-  val lst:List[Int] = 1::2::3::4::5::6::7::8::Nil;System.out.println("""lst  : List[Int] = """ + $show(lst ));$skip(16); val res$0 = 
-  imprimir(lst);System.out.println("""res0: Boolean = """ + $show(res$0))}
+  };System.out.println("""imprimir: (tablero: List[Int])Boolean""");$skip(336); 
+  
+  /*
+  	EN: Inserts element in a given position of the table.
+  	ES: Inserta un element en una posición dada del tablero.
+  */
+  def poner(tablero:List[Int], color:Int, posicion:Int) :List[Int] =
+	{
+		posicion match{
+			case 0 => color::tablero.tail
+  		case posicion => tablero.head::poner(tablero.tail, color, posicion - 1)
+		}
+	};System.out.println("""poner: (tablero: List[Int], color: Int, posicion: Int)List[Int]""");$skip(199); 
+	
+	/*
+		EN: Generates an empty table.
+		ES: Genera un tablero vacío.
+	*/
+	def generarv(longitud:Int): List[Int] = longitud match{
+		case 0 => return Nil
+		case longitud => 0::generarv(longitud-1)
+	};System.out.println("""generarv: (longitud: Int)List[Int]""");$skip(354); 
+  
+  /*
+  	EN: Inserts random number into the table.
+  	ES: Inserta valores aleatorios en el tablero. Falta comprobar el nivel de dificultad para
+  	definir el rango de valores.
+  */
+  def generarale(tablero:List[Int]):List[Int] =
+  {
+  	if(tablero.isEmpty) return Nil
+  	val rand = scala.util.Random
+  	(rand.nextInt(7)+1)::generarale(tablero.tail)
+  };System.out.println("""generarale: (tablero: List[Int])List[Int]""");$skip(38); 
+  
+  val lst:List[Int] = generarv(10);System.out.println("""lst  : List[Int] = """ + $show(lst ));$skip(28); val res$0 = 
+  imprimir(generarale(lst));System.out.println("""res0: Boolean = """ + $show(res$0))}
 }
--- JewelHeroesScala/src/JewelHeroesScala.sc
+++ JewelHeroesScala/src/JewelHeroesScala.sc
@@ -1,6 +1,11 @@
 object JewelHeroesScala {
   println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
   
+  /*
+  	EN: Function that prints the table to the screen. Still need to add a way to skip lines.
+  	ES: Función que escribe el tablero por pantalla. Todavía hay que añadir una forma de saltar
+  	líneas.
+  */
   def imprimir(tablero:List[Int]) : Boolean ={
   	if(tablero.isEmpty) return true
   	tablero.head match{
@@ -22,7 +27,41 @@ object JewelHeroesScala {
   		 imprimir(tablero.tail)
   	}
   }                                               //> imprimir: (tablero: List[Int])Boolean
-  val lst:List[Int] = 1::2::3::4::5::6::7::8::Nil //> lst  : List[Int] = List(1, 2, 3, 4, 5, 6, 7, 8)
-  imprimir(lst)                                   //> A	R	N	V	P	M	G	B	res0: Boolean = 
-                                                  //| true
+  
+  /*
+  	EN: Inserts element in a given position of the table.
+  	ES: Inserta un element en una posición dada del tablero.
+  */
+  def poner(tablero:List[Int], color:Int, posicion:Int) :List[Int] =
+	{
+		posicion match{
+			case 0 => color::tablero.tail
+  		case posicion => tablero.head::poner(tablero.tail, color, posicion - 1)
+		}
+	}                                         //> poner: (tablero: List[Int], color: Int, posicion: Int)List[Int]
+	
+	/*
+		EN: Generates an empty table.
+		ES: Genera un tablero vacío.
+	*/
+	def generarv(longitud:Int): List[Int] = longitud match{
+		case 0 => return Nil
+		case longitud => 0::generarv(longitud-1)
+	}                                         //> generarv: (longitud: Int)List[Int]
+  
+  /*
+  	EN: Inserts random number into the table.
+  	ES: Inserta valores aleatorios en el tablero. Falta comprobar el nivel de dificultad para
+  	definir el rango de valores.
+  */
+  def generarale(tablero:List[Int]):List[Int] =
+  {
+  	if(tablero.isEmpty) return Nil
+  	val rand = scala.util.Random
+  	(rand.nextInt(7)+1)::generarale(tablero.tail)
+  }                                               //> generarale: (tablero: List[Int])List[Int]
+  
+  val lst:List[Int] = generarv(10)                //> lst  : List[Int] = List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+  imprimir(generarale(lst))                       //> G	P	G	R	M	P	V	M	P	N	
+                                                  //| res0: Boolean = true
 }
