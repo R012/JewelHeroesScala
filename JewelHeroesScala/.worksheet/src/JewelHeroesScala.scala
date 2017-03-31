e8633d315cde9efa42742f362354efcb2876f59f
R012 <mario.cobos@edu.uah.es>
2017-03-31 09:14:52 +0200

    Created function imprimir
    We can print, to some extent

--- JewelHeroesScala/.worksheet/src/JewelHeroesScala.scala
+++ JewelHeroesScala/.worksheet/src/JewelHeroesScala.scala
@@ -1,3 +1,27 @@
 object JewelHeroesScala {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(69); 
-  println("Welcome to the Scala worksheet")}
+  println("Welcome to the Scala worksheet");$skip(557); 
+  
+  def imprimir(tablero:List[Int]) : Boolean ={
+  	if(tablero.isEmpty) return true
+  	tablero.head match{
+  		case 1 => print("A\t")
+  		 imprimir(tablero.tail)
+  		case 2 => print("R\t")
+  		 imprimir(tablero.tail)
+  		case 3 => print("N\t")
+  		 imprimir(tablero.tail)
+  		case 4 => print("V\t")
+  		 imprimir(tablero.tail)
+  		case 5 => print("P\t")
+  		 imprimir(tablero.tail)
+  		case 6 => print("M\t")
+  		 imprimir(tablero.tail)
+  		case 7 => print("G\t")
+  		 imprimir(tablero.tail)
+  		case 8 => print("B\t")
+  		 imprimir(tablero.tail)
+  	}
+  };System.out.println("""imprimir: (tablero: List[Int])Boolean""");$skip(50); 
+  val lst:List[Int] = 1::2::3::4::5::6::7::8::Nil;System.out.println("""lst  : List[Int] = """ + $show(lst ));$skip(16); val res$0 = 
+  imprimir(lst);System.out.println("""res0: Boolean = """ + $show(res$0))}
 }
--- JewelHeroesScala/bin/JewelHeroesScala.sc
+++ JewelHeroesScala/bin/JewelHeroesScala.sc
@@ -1,3 +1,28 @@
 object JewelHeroesScala {
   println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+  
+  def imprimir(tablero:List[Int]) : Boolean ={
+  	if(tablero.isEmpty) return true
+  	tablero.head match{
+  		case 1 => print("A\t")
+  		 imprimir(tablero.tail)
+  		case 2 => print("R\t")
+  		 imprimir(tablero.tail)
+  		case 3 => print("N\t")
+  		 imprimir(tablero.tail)
+  		case 4 => print("V\t")
+  		 imprimir(tablero.tail)
+  		case 5 => print("P\t")
+  		 imprimir(tablero.tail)
+  		case 6 => print("M\t")
+  		 imprimir(tablero.tail)
+  		case 7 => print("G\t")
+  		 imprimir(tablero.tail)
+  		case 8 => print("B\t")
+  		 imprimir(tablero.tail)
+  	}
+  }                                               //> imprimir: (tablero: List[Int])Boolean
+  val lst:List[Int] = 1::2::3::4::5::6::7::8::Nil //> lst  : List[Int] = List(1, 2, 3, 4, 5, 6, 7, 8)
+  imprimir(lst)                                   //> A	R	N	V	P	M	G	B	res0: Boolean = 
+                                                  //| true
 }
